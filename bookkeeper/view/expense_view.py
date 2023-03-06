3e7364c3890e5e3b6f57547b82c21173b27d805f
MakhrDee <125173977+MakhrDee@users.noreply.github.com>
2023-03-06 19:51:42 +0300

    Совмещены категории с расходами

--- bookkeeper/view/expense_view.py
+++ bookkeeper/view/expense_view.py
@@ -36,14 +36,16 @@ class MainWindow(QtWidgets.QMainWindow):
         self.expenses_grid = QtWidgets.QTableView()
         self.layout.addWidget(self.expenses_grid)
         self.layout.addWidget(QLabel('Бюджет'))
-        self.layout.addWidget(QLabel('<TODO: таблица бюджета>\n\n\n\n\n\n\n\n'))
+        #self.layout.addWidget(QLabel('<TODO: таблица бюджета>\n\n\n\n\n\n\n\n'))
+        self.budget = QtWidgets.QTableView()
+        self.layout.addWidget(self.budget)
 
 
         self.bottom_controls = QGridLayout()
 
         self.bottom_controls.addWidget(QLabel('Сумма'), 0, 0)
 
-        self.amount_line_edit = QLineEdit()
+        self.amount_line_edit = QLineEdit()  # TODO: добавить валидатор
 
         self.bottom_controls.addWidget(self.amount_line_edit, 0, 1)
         self.bottom_controls.addWidget(QLabel('Категория'), 1, 0)
@@ -80,4 +82,10 @@ class MainWindow(QtWidgets.QMainWindow):
             self.category_dropdown.addItem(tup[1], tup[0])
 
     def on_expense_add_button_clicked(self, slot):
-            self.expense_add_button.clicked.connect(slot)
+        self.expense_add_button.clicked.connect(slot)
+
+    def get_amount(self) -> float:
+        return float(self.amount_line_edit.text())  # TODO: обработка исключений
+
+    def get_selected_cat(self) -> int:
+        return self.category_dropdown.itemData(self.category_dropdown.currentIndex())
