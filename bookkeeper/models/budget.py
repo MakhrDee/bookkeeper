66f519c3c744bf4debfa6de85f0527c853922315
MakhrDee <125173977+MakhrDee@users.noreply.github.com>
2023-03-06 16:39:16 +0300

    Добавлен каркас расчета бюджета

---
+++ bookkeeper/models/budget.py
@@ -0,0 +1,24 @@
+from bookkeeper.repository.sqlite_repository import SQLiteRepository
+from bookkeeper.models.expense import Expense
+
+
+class Budget:
+    """
+    TODO: DOCSTRING
+    """
+
+    def day_budget(self):
+        """
+        Выставляется бюджет на день в таблице
+        """
+
+    def week_budget(self):
+        """
+        Выставляется бюджет на неделю в таблице
+        """
+
+    def month_budget(self):
+        """
+        Выставляется бюджет на месяц в таблице
+        """
+
