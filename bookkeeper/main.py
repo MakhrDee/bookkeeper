053d2ae0f437b988e49bb7f380b9613784df47c5
MakhrDee <125173977+MakhrDee@users.noreply.github.com>
2023-03-06 16:44:48 +0300

    Добавлен expense_presenter.py

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication
-
+from presenter.expense_presenter import ExpensePresenter
+from models.expense import Expense
 from view.expense_view import MainWindow
 
 import sys
@@ -7,6 +8,8 @@ import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
+    view = MainWindow()
+    model = None  # TODO: здесь должна быть модель
+    window = ExpensePresenter(model, view)
     window.show()
     app.exec_()
