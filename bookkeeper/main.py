ad77a4309a8f703e06973086a5c710b80cf82731
MakhrDee <125173977+MakhrDee@users.noreply.github.com>
2023-03-06 16:36:14 +0300

    Добавлен основной исполнительный файл

---
+++ bookkeeper/main.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QApplication
+
+from view.expense_view import MainWindow
+
+import sys
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec_()
