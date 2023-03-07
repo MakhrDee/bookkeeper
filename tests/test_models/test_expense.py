fa638c8c5245749a329271152bfff3b28ed9efc2
MakhrDee <125173977+MakhrDee@users.noreply.github.com>
2023-03-07 21:43:03 +0300

    Добавлен обязательный comment

--- tests/test_models/test_expense.py
+++ tests/test_models/test_expense.py
@@ -19,12 +19,13 @@ def test_create_with_full_args_list():
 
 
 def test_create_brief():
-    e = Expense(100, 1)
+    e = Expense(100, 1, 'test')
     assert e.amount == 100
     assert e.category == 1
+    assert e.comment == 'test'
 
 
 def test_can_add_to_repo(repo):
-    e = Expense(100, 1)
+    e = Expense(100, 1, 'test')
     pk = repo.add(e)
     assert e.pk == pk
